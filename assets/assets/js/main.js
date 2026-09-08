87f5cda239705b8918b53c16d57cad49124083af
drtfkjr <drtroykellyjr@gmail.com>
2026-09-07 22:58:51 -0400

    Create main.js

---
+++ assets/assets/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const dropbtn = document.querySelector('.dropbtn');
+  const dropdown = document.querySelector('.dropdown');
+
+  dropbtn.addEventListener('click', function () {
+    dropdown.classList.toggle('open');
+  });
+
+  document.addEventListener('click', function (e) {
+    if (!dropdown.contains(e.target)) {
+      dropdown.classList.remove('open');
+    }
+  });
+});
